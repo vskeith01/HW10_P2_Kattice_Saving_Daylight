7073f908c2907f200f21d84332897abb4bc3bd5d
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-11-02 02:06:38 +0000

    Wokring code. Pre kattice test

---
+++ .ccls-cache/@home@runner@HW10P2KatticeSavingDaylight/main.cpp
@@ -0,0 +1,69 @@
+ #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+struct daylightInfo{
+string month;
+int day;
+int year;
+int AMhour;
+int AMmin;
+int PMhour;
+int PMmin;
+};
+
+//////////////prototyped functions ////////////
+int hours(daylightInfo data[], int N, int index);
+int minutes(daylightInfo data[], int N, int index);
+
+int main() {
+  char throwaway;
+  int N;
+  cin >> N;
+  daylightInfo data[N];
+
+  for (int i = 0; i < N; i++){
+    cin >> data[i].month >> data[i].day >> data[i].year >> data[i].AMhour >> data[i].AMmin >> data[i].PMhour >> data[i].PMmin;
+  }
+
+  for (int j = 0; j < N; j++)
+    {
+      cout << data[j].month << " " << data[j].day << " " << data[j].year << " " << hours(data, N, j) << " hours " << minutes(data, N, j) << " minutes" << endl;
+      
+    }
+  
+}
+
+int hours(daylightInfo data[], int N, int index)
+{
+  int hours;
+  
+    if (data[index].PMmin < data[index].AMmin)
+    {
+      hours = data[index].PMhour - data[index].AMhour - 1;
+    }
+    else
+      hours = data[index].PMhour - data[index].AMhour;
+
+  return hours;
+}
+
+
+
+int minutes(daylightInfo data[], int N, int index)
+{
+
+  int minutes;
+  
+  if (data[index].PMmin < data[index].AMmin)
+    {
+      minutes = data[index].PMmin - data[index].AMmin + 60;
+    }
+    else
+      minutes = data[index].PMmin - data[index].AMmin;
+
+return minutes;  
+}
